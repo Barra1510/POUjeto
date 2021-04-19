f53022767920005844a3ab7e37e502daabcd15a8
barra1510 <gabrielbarrinha15@gmail.com>
2021-04-19 02:18:26 -0300

    Vuforia implementada, com troca de telas

---
+++ PrOUJETO/Assets/Barrinha/Scripts/Actions.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Actions : MonoBehaviour
+{
+    #region Declaration
+    [SerializeField] Vector3 scale;
+    [Header("Controlador de crescimento")]
+    [SerializeField] float growValue;
+    #endregion
+    void Start()
+    {
+        scale = transform.localScale;
+    }
+
+    
+    void Update()
+    {
+    }
+    //Crescer
+    public void Grow()
+    {
+        scale.x += growValue;
+        scale.y += growValue;
+        scale.z += growValue;
+        transform.localScale = scale;
+    }
+
+    //Diminuir
+    public void Shrink()
+    {
+        scale.x -= growValue;
+        scale.y -= growValue;
+        scale.z -= growValue;
+        transform.localScale = scale;
+    }
+}
---
+++ PrOUJETO/Assets/Barrinha/Scripts/ScreenTransition.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class ScreenTransition : MonoBehaviour
+{
+    [SerializeField] GameObject[] screenList;
+    [SerializeField] int actualScreen = 0;
+    public void GoToMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void GoToAR()
+    {
+        SceneManager.LoadScene("Teste Vuforia");
+    }
+
+    public void GoLeft()
+    {
+        screenList[actualScreen].SetActive(false);
+        if (actualScreen != 0)
+        {
+            actualScreen -= 1;
+            screenList[actualScreen].SetActive(true);
+        }
+        if(actualScreen == 0)
+        {
+            screenList[actualScreen].SetActive(true); ;
+        }
+    }
+
+    public void GoRight()
+    {
+        screenList[actualScreen].SetActive(false);
+        if (actualScreen != 2)
+        {
+            actualScreen += 1;
+            screenList[actualScreen].SetActive(true);
+        }
+        if (actualScreen == screenList.Length - 1)
+        {
+            screenList[actualScreen].SetActive(true); ;
+        }
+    }
+}
