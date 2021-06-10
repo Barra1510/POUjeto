ab71c2aa91b4dbe8a8b642dd9d80de7a37c73e09
LuizPismelJr <61069801+LuizPismelJr@users.noreply.github.com>
2021-06-10 04:01:36 -0300

    tu vai tbm saporra?

---
+++ PrOUJETO/Assets/Barrinha/Scripts/AnimationScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationScript : MonoBehaviour
+{
+    [SerializeField] Animator anim;
+    void Update()
+    {
+        ChangeAnimation();
+    }
+    
+    void ChangeAnimation()
+    {
+        if(PlayerPrefs.GetFloat("Health") < 10)
+        {
+            anim.SetBool("Sick", true);
+            anim.SetBool("Sad", false);
+            anim.SetBool("Hungry", false);
+        }
+        if(PlayerPrefs.GetFloat("Energy") < 10)
+        {
+            anim.SetBool("Sad", true);
+            anim.SetBool("Sick", false);
+            anim.SetBool("Hungry", false);
+        }
+        if(PlayerPrefs.GetFloat("Hunger") < 10)
+        {
+            anim.SetBool("Hungry", true);
+            anim.SetBool("Sick", false);
+            anim.SetBool("Sad", false);
+        }
+        anim.SetBool("Sick", false);
+        anim.SetBool("Sad", false);
+        anim.SetBool("Hungry", false);
+
+    }
+}
